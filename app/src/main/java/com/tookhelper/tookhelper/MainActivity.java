bf8d7510f305ca302e91092e58d46f1115f3e44c
ismailtang <ismailtangur@gmail.com>
2020-10-07 10:36:25 +0300

    commit

--- app/src/main/java/com/tookhelper/tookhelper/MainActivity.java
+++ app/src/main/java/com/tookhelper/tookhelper/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        TookAnalytics.Event("asdasd", "497FaF0112932", this);
+        TookAnalytics.Event("Test", "497FaF0112932", this);
 
 
     }
